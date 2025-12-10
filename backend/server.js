f3ef6e005273d2cc0eb265c24f8612576ab09b92
Markusdeatrius <ljhb@email.cz>
2025-12-10 11:51:14 +0100

    dbRework

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,12 @@ const path = require('path');
 const seed = require('./database/seed');
 const db = require('./database/db');
 const session = require('express-session');
+const checkLoginRouter = require('./api/check-login');
+const logoutRouter = require('./api/logout');
 
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
-
-app.get("/test", (req, res) => {
-    res.json({ ok: true });
-});
 
 app.use(cors({ origin: 'http://localhost:8081', credentials: true }));
 app.use(express.json());
@@ -23,6 +21,15 @@ app.use(session({
     saveUninitialized: false,
     cookie: { secure: false, httpOnly: true, maxAge: 86400000 }
 }));
+
+
+app.get("/test", (req, res) => {
+    res.json({ ok: true });
+});
+
+
+app.use('/api/check-login', checkLoginRouter);
+app.use('/api/logout', logoutRouter);
 
 (async () => {
     try {
